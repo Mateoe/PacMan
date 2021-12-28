6d44d1f7ca5cc22ddebec0df5842f8bb43f27882
mateoe <maespinallo@unal.edu.co>
2021-12-27 19:13:44 -0500

    Colision de pacman y cambio en las animaciones

--- Pacmancito/Assets/Resources/Scripts/PacManController.cs
+++ Pacmancito/Assets/Resources/Scripts/PacManController.cs
@@ -9,9 +9,15 @@ public class PacManController : MonoBehaviour
     public float velocity = 4.0f;
     public GameObject _PacManBody;
 
+    public GameObject _CollisionDetector;
+    public GameObject[] _Walls;
+
+    public bool _isColliding;
+
     //Direccion del movimiento
     public Vector3 direction;
     Rigidbody _rb;
+    Sprite _PacManSprite;
 
     void Start()
     {   
@@ -19,18 +25,31 @@ public class PacManController : MonoBehaviour
         direction = Vector3.zero;
         _rb = GetComponent<Rigidbody>();
         _PacManBody = GameObject.Find("PacManBody");
+        _CollisionDetector = GameObject.Find("CollisionDetector");
+        _Walls = GameObject.FindGameObjectsWithTag("Wall");
+        _PacManSprite = _PacManBody.GetComponent<SpriteRenderer>().sprite;
+        _isColliding = true;
+
     }
 
     void Update()
     {   
         //En cada frame se lee el movimiento del jugador
         GetDirection();
-        //Se plica el movimiento  
+        //Se plica el movimiento
+        IsTouchingWall();
+
+        if(_isColliding){
+            _PacManBody.GetComponent<Animator>().enabled = false;
+            _PacManBody.GetComponent<SpriteRenderer>().sprite = _PacManSprite;
+        }else{
+           _PacManBody.GetComponent<Animator>().enabled = true;
+        }
     }
 
     void FixedUpdate()
     {
-        move();
+        Move();
     }
 
     //se obtiene la direccion del movimiento del usuario con las teclas WASD o las flechas
@@ -54,20 +73,33 @@ public class PacManController : MonoBehaviour
         }
 
         _PacManBody.transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(90,0,90);
+        
         return direction;
         
     }        
 
-    void move()
+    void Move()
     {    
         Vector3 _targetdirection = direction*velocity*Time.deltaTime;
+
 
         Vector3 _targetposition = transform.position + _targetdirection;
 
         _rb.MovePosition(_targetposition);
 
-        Vector3 _roundedposition = new Vector3(Convert.ToSingle(Math.Round(transform.position.x,1)),Convert.ToSingle(Math.Round(transform.position.y,1)),Convert.ToSingle(Math.Round(transform.position.z,1)));
-   
+    }
+
+    public void IsTouchingWall(){
+        bool _anyColliding = false;
+        foreach (GameObject wall in _Walls)
+        {
+            if(_CollisionDetector.GetComponent<Collider>().bounds.Intersects(wall.GetComponent<Collider>().bounds)){
+                _anyColliding = true;
+            }
+        }
+
+        _isColliding = _anyColliding;
+
     }
 
 }
