92cda9eb201888b5aa2b5559367a96b2b320c7be
mateoe <maespinallo@unal.edu.co>
2022-01-03 10:51:03 -0500

    Actualización masiva y nuevo sistema de movimiento

--- Pacmancito/Assets/Resources/Scripts/AI/StateMachine.cs
+++ Pacmancito/Assets/Resources/Scripts/AI/StateMachine.cs
@@ -4,15 +4,112 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GhostID _ghostID;
+
+    [SerializeField]
+    private BlinkyChase _blinkyChase;
+
+    [SerializeField]
+    private InkyChase _inkyChase;
+
+    [SerializeField]
+    private PinkyChase _pinkyChase;
+
+    [SerializeField]
+    private ClydeChase _clydeChase;
+
+    [SerializeField]
+    private Vulnerable _vulnerableScript;
+
+    [SerializeField]
+    private Dispersion _dispersionScript;
+    
+
     void Start()
     {
-        
-    }
+        if(_ghostID == GhostID.Blinlky){
+            _blinkyChase= gameObject.GetComponent<BlinkyChase>();
+        }
+        if(_ghostID == GhostID.Inky){
+            _inkyChase= gameObject.GetComponent<InkyChase>();
+        }
+        if(_ghostID == GhostID.Pinky){
+            _pinkyChase= gameObject.GetComponent<PinkyChase>();
+        }
+        if(_ghostID == GhostID.Clyde){
+            _clydeChase= gameObject.GetComponent<ClydeChase>();
+        }
 
-    // Update is called once per frame
+        _vulnerableScript = gameObject.GetComponent<Vulnerable>();
+
+        _dispersionScript = gameObject.GetComponent<Dispersion>();
+
+        EnterDispersion();
+    }
     void Update()
     {
-        
+        if(Vector3.Magnitude(transform.position-_dispersionScript._dispersiontarget.transform.position)<0.05){
+            EnterChase();
+        }
     }
+
+    void EnterChase()
+    {
+        _dispersionScript.enabled = false;
+        _vulnerableScript.enabled = false;
+
+        if(_blinkyChase != null){
+            _blinkyChase.enabled = true;
+        }
+        if(_inkyChase != null){
+            _inkyChase.enabled = true;
+        }
+        if(_pinkyChase != null){
+            _pinkyChase.enabled = true;
+        }
+        if(_clydeChase != null){
+            _clydeChase.enabled = true;
+        }
+
+    }
+
+    void EnterVulnerable()
+    {
+        _dispersionScript.enabled = false;
+        _vulnerableScript.enabled = true;
+
+        if(_blinkyChase != null){
+            _blinkyChase.enabled = false;
+        }
+        if(_inkyChase != null){
+            _inkyChase.enabled = false;
+        }
+        if(_pinkyChase != null){
+            _pinkyChase.enabled = false;
+        }
+        if(_clydeChase != null){
+            _clydeChase.enabled = false;
+        }
+    }
+
+    void EnterDispersion()
+    {
+        _dispersionScript.enabled = true;
+        _vulnerableScript.enabled = false;
+
+        if(_blinkyChase != null){
+            _blinkyChase.enabled = false;
+        }
+        if(_inkyChase != null){
+            _inkyChase.enabled = false;
+        }
+        if(_pinkyChase != null){
+            _pinkyChase.enabled = false;
+        }
+        if(_clydeChase != null){
+            _clydeChase.enabled = false;
+        }
+    }
+
 }
--- Pacmancito/Assets/Resources/Scripts/Pellet/EnergizerPellet.cs
+++ Pacmancito/Assets/Resources/Scripts/Pellet/EnergizerPellet.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dispersion : MonoBehaviour
+public class EnergizerPellet : Pellet
 {
-    // Start is called before the first frame update
+    
     void Start()
-    {
-        
+    { 
+        points = 30;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
